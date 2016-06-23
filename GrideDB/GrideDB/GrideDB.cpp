b91e06b886f6535d904f902e55e6cf5047c1fc3d
amanuel2 <amanuelb43@gmail.com>
2016-06-23 07:38:00 -0400

    New Changes

--- GrideDB/GrideDB/GrideDB.cpp
+++ GrideDB/GrideDB/GrideDB.cpp
@@ -5,22 +5,39 @@ int GrideDB::numDb = 0;
 GrideDB::GrideDB(const str& db_name) :DB_NAME(db_name)
 {
 	numDb++;
+	initalized = false;
 }
 
 void GrideDB::init()
 {
-	initalized = true;
-	DB_INFO_OBJ.DB_NAME = DB_NAME;
-	//TODO : DB KEY RANDOM GENERATOR
-	DB_INFO_OBJ.DB_KEY = 123;
+	if (initalized == true) {
+		cout << " ERR : ALREADY INITALIZED DATABASE" << endl;
+	}
+	else
+	{
+		initalized = true;
+		DB_INFO_OBJ.DB_NAME = DB_NAME;
+		//TODO : DB KEY RANDOM GENERATOR
+		DB_INFO_OBJ.DB_KEY = 123;
+		DB_FILE.open(DB_INFO_OBJ.DB_NAME + lexical_cast<str>(DB_INFO_OBJ.DB_KEY) + "_GRIDEDB" + lexical_cast<str>(EXTENSION));
+		DB_FILE << "_______" << DB_INFO_OBJ.DB_NAME << lexical_cast<str>(DB_INFO_OBJ.DB_KEY) << "______" << endl;
+	}
 }
 
 //{Param} Database add
 //Adds to end of database structure.
-void GrideDB::add(str key,  str val)
+void GrideDB::add(const str & key, const str &val)
 {
-		dbStr.insert(key, val);
-}
+	if (initalized == false)
+	{
+		cout << "DATABASE NOT INITALIZED" << endl;
+		exit(1);
+	}
+	else
+	{
+
+	}
+};
 
 GrideDB::~GrideDB(void)
 {
--- GrideDB/GrideDB/GrideDB.h
+++ GrideDB/GrideDB/GrideDB.h
@@ -2,12 +2,18 @@
 #include<iostream>
 #include<string>
 #include<map>
+#include<fstream>
+#include<boost/lexical_cast.hpp>
+
+#define EXTENSION ".gb"
 
 typedef std::string str;
 
 using std::cout;
 using std::endl;
 using std::string;
+
+using boost::lexical_cast;
 
 #define MAX 32
 
@@ -26,15 +32,16 @@ public:
 	{
 		str DB_NAME;
 		long long DB_KEY;
-	};
+	}DB_INFO_OBJ;
 
 	GrideDB(const str& db_name);
 	~GrideDB(void);
 	void init(void);
-	void add( str  key,  str val);
+
+	void add(const str & key, const str &val);
 private:
-	DB_INFO DB_INFO_OBJ;
 	str DB_NAME;
+	std::ofstream DB_FILE;
 	static int numDb;
 	bool initalized;
 	std::map<string, string> dbStr;
